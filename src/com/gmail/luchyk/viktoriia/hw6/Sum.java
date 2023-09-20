06e04344e1206ef607df37d1997a157ea045a0c8
Viktoriia Luchyk <viktoriia.luchyk@gmail.com>
2023-09-20 20:12:06 +0300

    hw6: two more realizations of the twoSum method

--- src/com/gmail/luchyk/viktoriia/hw6/Sum.java
+++ src/com/gmail/luchyk/viktoriia/hw6/Sum.java
@@ -1,6 +1,7 @@
 package com.gmail.luchyk.viktoriia.hw6;
 
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Sum {
     public static void main(String[] args) {
@@ -25,17 +26,52 @@ public class Sum {
         System.out.println("Indices of the two numbers that add up to target: " + Arrays.toString(thirdResult));
     }
 
-    public static int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        for (int i = 0; i < nums.length; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if (nums[i] + nums[j] == target) {
-                    result[0] = i;
-                    result[1] = j;
-                    return result;
+        public static int[] twoSum(int[] nums, int target) {
+            int[] result = new int[2];
+            for (int i = 0; i < nums.length; i++) {
+                for (int j = i + 1; j < nums.length; j++) {
+                    if (nums[i] + nums[j] == target) {
+                        result[0] = i;
+                        result[1] = j;
+                        return result;
+                    }
                 }
             }
+            return result;
         }
-        return result;
+    /*
+    public static int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> collector = new HashMap<>();
+        for (int number : nums) {
+            if (collector.containsKey(number)) {
+                return new int[]{number, collector.get(number)};
+            } else {
+                collector.put(target - number, number);
+            }
+        }
+        return null;
     }
+     */
+
+    /*
+    public static int[] twoSum(int[] nums, int target) {
+        // 3, 10, 8, 45, 5, target = 15
+        // 3, 5, 8, 10, 45
+        // 3, 5, 10, 25, 43, 44, 45
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int total = nums[left] + nums[right];
+            if (total > target) {
+                right--;
+            } else if (total < target) {
+                left++;
+            } else {
+                return new int[] {left, right};
+            }
+        }
+        return null;
+    }
+     */
 }

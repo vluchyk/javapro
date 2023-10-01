c17fb7a96fedcb6fd06c3ede2696036385c74df5
Viktoriia Luchyk <viktoriia.luchyk@gmail.com>
2023-10-02 01:26:40 +0300

    hw8: stream api, implementation minPriceByType, lastNCreated, totalPrice, and groupByType methods

--- src/com/gmail/luchyk/viktoriia/hw8/Main.java
+++ src/com/gmail/luchyk/viktoriia/hw8/Main.java
@@ -2,7 +2,9 @@ package com.gmail.luchyk.viktoriia.hw8;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -18,6 +20,19 @@ public class Main {
 
         printInfo(byTypeWithDiscount(products, "Book", 10));
         System.out.println();
+
+        minPriceByType(products, "Book");
+        System.out.println();
+        minPriceByType(products, "Magazine");
+        System.out.println();
+
+        printInfo(lastNCreated(products, 3));
+        System.out.println();
+
+        totalPrice(products, 2023, "Book", 200);
+        System.out.println();
+
+        System.out.println(groupByType(products));
     }
 
     private static void printInfo(List<Product> products) {
@@ -36,16 +51,48 @@ public class Main {
 
     private static List<Product> byTypeWithDiscount(List<Product> products, String type, int percentage) {
         System.out.printf("___ Result of Type = %s, with discount = %d: ___\n", type, percentage);
-        List<Product> sales = products.stream()
+        return products.stream()
+                .filter(t -> t.getType().equalsIgnoreCase(type))
                 .filter(Product::isDiscount)
-                //p.getPrice() * percentage / 100) //TO-DO
+                .map(product -> {
+                    product.setPrice(product.getPrice() - product.getPrice() * percentage / 100);
+                    return product;
+                })
                 .collect(Collectors.toList());
+    }
 
-        for (Product sale : sales) {
-            sale.setPrice(sale.getPrice() - sale.getPrice() * percentage / 100);
-        }
+    private static void minPriceByType(List<Product> products, String type) {
+        System.out.printf("___ Result of Type = %s with min price: ___\n", type);
+        products.stream()
+                .filter(t -> t.getType().equalsIgnoreCase(type))
+//                .sorted(Comparator.comparing(Product::getPrice))
+//                .findFirst()
+                .min(Comparator.comparing(Product::getPrice))
+                .ifPresentOrElse(System.out::println, () -> System.out.printf("The product %s is not found.\n", type));
+    }
 
-        return sales;
+    private static List<Product> lastNCreated(List<Product> products, int count) {
+        System.out.printf("___ Result of last %d created products: ___\n", count);
+        return products.stream()
+                .sorted(Comparator.comparing(Product::getCreated).reversed())
+                .limit(count)
+                .collect(Collectors.toList());
+    }
+
+    private static void totalPrice(List<Product> products, int year, String type, double price) {
+        System.out.printf("___ Result of total price created in %d of Type = %s, and price > %.2f:  ___\n", year, type, price);
+        double result = products.stream()
+                .filter(d -> d.getCreated().getYear() == year)
+                .filter(t -> t.getType().equalsIgnoreCase(type))
+                .filter(p -> p.getPrice() > price)
+                .collect(Collectors.summingDouble(Product::getPrice));
+        System.out.printf("Total Price: %.2f.\n", result);
+    }
+
+    private static Map<String, List<Product>> groupByType(List<Product> products) {
+        System.out.println("___ Result of products grouped by type: ___");
+        return products.stream()
+                .collect(Collectors.groupingBy(Product::getType));
     }
 
     private static List<Product> getProducts() {
